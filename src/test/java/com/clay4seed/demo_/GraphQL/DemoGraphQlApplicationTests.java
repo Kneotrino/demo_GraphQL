0263fc7f996925b7d4536dd41c1a322a42caf948
Neutrino Kusrorong <kneotrino@yahoo.com>
2019-11-08 14:07:49 +0700

    Initial commit

---
+++ src/test/java/com/clay4seed/demo_/GraphQL/DemoGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clay4seed.demo_.GraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
